554339e0bacea3a59d86b74e9dad23e1750e5543
Prince Donkor <Prince.donkor74@gmail.com>
2018-12-18 19:47:53 -0600

    added react-redux firebase and redux-firebase, not i am not yet to write to my data base

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -2,12 +2,17 @@ const initState = {
     projects: [
         {id: '1', title: 'this is a text for the title 1', content: 'blah blah blah'},
         {id: '2', title: 'this is a text for the title 2', content: 'blah blah blah'},
-        {id: '2', title: 'this is a text for the title 3', content: 'blah blah blah'}
+        {id: '3', title: 'this is a text for the title 3', content: 'blah blah blah'}
     ]
 }
 
 const projectReducer = (state = initState, action) => {
-    return state
+    switch(action.type){
+        case 'CREATE_PROJECT':
+        console.log('created project', action.project)
+        default:
+        return state
+    }
 }
 
 export default projectReducer
