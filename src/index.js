f70e3b2990bf535d5aedf71f92aa1d174e390934
Prince Donkor <Prince.donkor74@gmail.com>
2018-12-19 21:30:15 -0600

    our application now waits for firebase to finish before showing

--- src/index.js
+++ src/index.js
@@ -15,17 +15,22 @@ const store = createStore(rootReducer,
   compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
+        reactReduxFirebase(fbConfig, {attachAuthIsReady : true})
   )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+store.firebaseAuthIsReady.then(() =>{
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  
+    document.getElementById("root")
+  );
 
-  document.getElementById("root")
-);
+})
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
