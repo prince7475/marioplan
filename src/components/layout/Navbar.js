cdf7aa5922a530fd8eb45713299e306fb1e3f944
Prince Donkor <Prince.donkor74@gmail.com>
2018-12-19 21:23:52 -0600

    navbar now changes if the user is logged in and logged out

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from 'react-redux'
-function Navbar() {
+function Navbar(props) {
+    const {auth} = props
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to="/" className="brand-logo">
                     MarioPlan
                 </Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {links}
             </div>
         </nav>
     );
 }
 const mapStateToProps = (state) => {
-    console.log(state)
     return {
-
+        auth: state.firebase.auth
     }
 }
 export default connect(mapStateToProps)(Navbar);
