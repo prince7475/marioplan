1bbe923633bb5804abde07285f4c3eeffbf9b3bc
Prince Donkor <Prince.donkor74@gmail.com>
2018-12-20 21:13:03 -0600

    now we can sign up users

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_SUCCESS, LOGIN_ERROR, SIGNOUT_SUCCESS } from "../actions/actionTypes";
+import { LOGIN_SUCCESS, LOGIN_ERROR, SIGNOUT_SUCCESS, SIGNUP_ERROR } from "../actions/actionTypes";
 
 const initState = {
     authError: null
@@ -23,6 +23,20 @@ const authReducer = (state = initState, action) => {
         case SIGNOUT_SUCCESS:
         console.log('sign out success ')
         return state
+
+        case SIGNOUT_SUCCESS:
+        console.log('sign up was a success')
+        return {
+            ...state, 
+            authError: null
+        }
+
+        case SIGNUP_ERROR:
+        console.log('sign up error')
+        return {
+            ...state,
+            authError: action.err.message
+        }
         default:
             return state
     }
