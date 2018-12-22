0d8204c445e483835125fe3f441b52e69600ad57
Prince Donkor <Prince.donkor74@gmail.com>
2018-12-22 13:31:05 -0600

    we can now get the user's information on firebase.profile

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,8 +4,8 @@ import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from 'react-redux'
 function Navbar(props) {
-    const {auth} = props
-    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+    const {auth, profile} = props
+    const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
@@ -18,8 +18,10 @@ function Navbar(props) {
     );
 }
 const mapStateToProps = (state) => {
+    console.log(state)
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        profile: state.firebase.profile
     }
 }
 export default connect(mapStateToProps)(Navbar);
